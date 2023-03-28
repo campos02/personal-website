3c474d7d095912d91bb4a17ca3e2389a32d7bef2
campos <viniciusca260@gmail.com>
2023-03-28 19:49:30 -0300

    Use models for database operations

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
+use App\Models\Artist;
+use App\Models\Album;
 
 class ArtistsController extends Controller
 {
     public function show() : View
     {
-        $listeningArtists = DB::table('artists')->where('category', 'Listening')->get();
-        $otherArtists = DB::table('artists')->where('category', 'Other')->get();
-        $albumsQuery = DB::table('albums')->get();
+        $listeningArtists = Artist::where('category', 'Listening')->get();
+        $otherArtists = Artist::where('category', 'Other')->get();
+        $albumsQuery = Album::all();
         $albums = $albumsQuery->groupBy('artist');
 
         return view('favoriteartists', [
