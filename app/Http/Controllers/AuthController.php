5a52ea6814bad8ccfdffed511d6500c8378c2bc6
campos <viniciusca260@gmail.com>
2023-04-18 00:24:53 -0300

    Specify web guard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,7 +44,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        Auth::logout();
+        Auth::guard('web')->logout();
     
         $request->session()->invalidate();
     
