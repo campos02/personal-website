452a738231e30d1353911559887e47fa5bb65692
campos <viniciusca260@gmail.com>
2024-01-21 18:26:48 -0300

    Update packages and add to bio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,9 @@
         <p>Hey, I'm campos and this is my personal website</p>
         <div class="m-4">
             <h1 class="text-3xl font-bold">A short bio</h2>
-            <p class="mt-1 dark:text-zinc-300">I'm a {{ $age }} year old brazilian interested in programming, electronics and music</p>
+            <p class="mt-1 dark:text-zinc-300">
+                I'm a {{ $age }} year old brazilian interested in programming, electronics and music and also a Computer Science student.
+            </p>
         </div>
     </div>
 </x-layout>
