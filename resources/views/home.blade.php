1225e4bec414bdb6f9bd41fb52f736280de01411
Frankie B <git@diskfloppy.me>
2024-02-15 21:15:43 +0000

    Correct closing h2 to h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="text-lg place-self-center text-center">
         <p>Hey, I'm campos and this is my personal website</p>
         <div class="m-4">
-            <h1 class="text-3xl font-bold">A short bio</h2>
+            <h1 class="text-3xl font-bold">A short bio</h1>
             <p class="mt-1 dark:text-zinc-300">
                 I'm a {{ $age }} year old brazilian interested in programming, electronics and music and also a Computer Science student.
             </p>
